a76e077f22da5220645d39fd48c42739f6f7ca0a
BytteL <lettybedard@gmail.com>
2020-02-01 13:26:18 -0500

    click event for both buttons

--- script.js
+++ script.js
@@ -2,14 +2,39 @@
 
 $(function (){
 
-  let apiKey = "api-key=nbrMMceZO1IOwwdSGsU9iknxjJ82Mhgs";
-  let apiSecret = "16LSk18wu5TAXNoC";
-  let queryString = "&q=puppies";
-  let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?${apiKey}${queryString}`;
+  // let apiKey = "api-key=nbrMMceZO1IOwwdSGsU9iknxjJ82Mhgs";
+  // let apiSecret = "16LSk18wu5TAXNoC";
+  // let queryString = "&q=puppies";
+  // let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?${apiKey}${queryString}`;
 
-  $.get(queryURL, function(response){
-    let results = response;
-    console.log(results);
+  // $.get(queryURL, function(response){
+  //   let results = response;
+  //   console.log(results);
+  // });
+
+  // DECLARATIONS
+  let apiKey = "api-key=nbrMMceZO1IOwwdSGsU9iknxjJ82Mhgs";
+  let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+  let queryString = "";
+
+  let $searchTerm = $("#searchTerm");
+  let $numRecords = $("#numRecords");
+  let $startYear = $("#startYear");
+  let $endYear = $("#endYear");
+
+
+  $(document).on("click", ".searchBtn", function({
+    
+  }));
+
+  $(document).on("click", ".clearBtn", function(){
+
+    [$searchTerm, $startYear, $endYear].forEach(elem =>{
+      elem.text("");
+    });
+    $numRecords.val("1");
   });
+
+
 
 });
